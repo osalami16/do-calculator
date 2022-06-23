3ba55e7b1b4154fc82a4f046edc928c7355643df
omolola taiwo <omololataiwo93@gmail.com>
2022-06-22 17:45:48 -0700

    first commit

---
+++ do.js
@@ -0,0 +1,18 @@
+
+var num1 = parseInt(prompt("first number 1 :"))
+var num2 = parseInt(prompt("second number 2 :"))
+var opr = prompt("operation")
+
+if(opr == "+")
+{
+ console.log(num1 + num2 )
+}else if(opr == "-")
+{
+ console.log(num1 - num2)
+}else if(opr == "*")
+{
+ console.log(num1 * num2)
+}else if(opr == "/")
+{
+ console.log(num1 / num2)
+}
